e33e4711e9eed5c6771f71b789affafa58f1ab52
Dylan Gardham <message4dylan@gmail.com>
2026-01-21 22:41:22 +0000

    separate dragged cable drawing in example

--- examples/drag_create_cable.rs
+++ examples/drag_create_cable.rs
@@ -43,12 +43,7 @@ impl eframe::App for MyEguiApp {
                 ui.add(Port::new(5usize));
             });
 
-            // add a port that you can create cables from
-            ui.add_space(150.0);
-            let port_response = ui.add(Port::new(6usize));
-            if port_response.drag_started() {
-                self.connected.push((Some(6usize), None));
-            }
+            let num_cables = self.connected.len();
 
             fn helper(port_index: &Option<usize>) -> Plug {
                 match port_index {
@@ -62,14 +57,12 @@ impl eframe::App for MyEguiApp {
             // });
             // ui.scope_builder(egui::UiBuilder::new(), add_contents);
 
-            let num_cables = self.connected.len();
-
             // draw existing cables
             for (index, (from, to)) in self.connected.iter_mut().enumerate() {
                 let mut my_cable = Cable::new(index, helper(from), helper(to));
-                if index + 1 == num_cables {
-                    my_cable = my_cable.forward_drag_response(&port_response);
-                }
+                // if index + 1 == num_cables {
+                //     my_cable = my_cable.forward_drag_response(&port_response);
+                // }
                 let mut cable_response = ui.add(my_cable);
 
                 if let Some(new_from) = cable_response.in_plug().connected_to() {
@@ -81,6 +74,29 @@ impl eframe::App for MyEguiApp {
 
                 // not checking for disconnected cables, so when you drop a cable, it gets put back
                 // you could handle it here if you wanted to allow floating plugs
+            }
+
+            // add a port that you can create cables from
+            ui.add_space(150.0);
+            let port_response = ui.add(Port::new(6usize));
+            if port_response.dragged() {
+                let dragged_cable_response = ui.add(
+                    Cable::new(num_cables, helper(&Some(6)), helper(&None))
+                        .forward_drag_response(&port_response),
+                );
+            }
+            if port_response.drag_stopped() {
+                let mut dragged_cable_response = ui.add(
+                    Cable::new(num_cables, helper(&Some(6)), helper(&None))
+                        .forward_drag_response(&port_response),
+                );
+                self.connected.push((
+                    Some(6usize),
+                    dragged_cable_response
+                        .out_plug()
+                        .connected_to()
+                        .map(|id| id.downcast_ref::<usize>().unwrap().clone()),
+                ));
             }
 
             ui.add_space(40.0);
